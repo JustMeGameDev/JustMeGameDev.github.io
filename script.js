3cef68ee9cab32b541fbf2636dd8b8bbf99da7d3
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2024-04-10 15:45:31 +0200

    bug fix

--- script.js
+++ script.js
@@ -42,7 +42,14 @@ function hideTooltip() {
 }
 function showPage(element) {
    const name = element.getAttribute('data-page') + ".html"
-    document.open(name)
+   fetch(name) // Path to your about.html file
+        .then(response => response.text())
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(err => {
+            console.error('Failed to load page: ', err);
+        });
 }
 
 
