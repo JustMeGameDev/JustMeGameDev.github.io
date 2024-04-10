0d3f17fb0c730b8c8848b220f5d0e87abfa217b7
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2024-04-10 15:10:43 +0200

    patch

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ function hideTooltip() {
     tooltipArea.style.opacity = '0';
 }
 function showPage(element) {
-    const name =element.getAttribute('data-page') + ".html";
-    window.location.href= name
+    const name = element.getAttribute('data-page') + ".html";
+    window.open(name)
 }
 
 
