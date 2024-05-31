5729437a7166ae4118d79a68c4c7f98b0dde882d
Kornee Hartlief <90724234+JustMeGameDev@users.noreply.github.com>
2024-05-31 13:12:44 +0200

    po-up

--- script.js
+++ script.js
@@ -97,6 +97,26 @@ document.querySelectorAll('.header-img').forEach(img => {
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 
+window.onload = function() {
+    var modal = document.getElementById("constructionModal");
+    var span = document.getElementsByClassName("close-btn")[0];
+
+    // Show the modal
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
 generateStars(200); // Generate 200 stars. Adjust the number as needed.
 
 
