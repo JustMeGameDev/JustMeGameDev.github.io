b0404578180f509f8d5a7f6795a99581474f2e72
JustMeGameDev <kwhartlief@st.noorderpoort.nl>
2024-06-11 10:47:15 +0200

    badges update

--- Portfolio.js
+++ Portfolio.js
@@ -3,6 +3,7 @@
 const projects = [
     {
         title: "Long Live Picho",
+        engine: "./img/Other/U_Cube_1C_Black.svg",
         category: "games",
         description: "This game is a prototype game made in a weekend by 4 students game development of the Secondary vocational education level 4.  we are proud of the workings of the game but visually it could be improved. It did win an award for the unique mechanics. (Global Game-jam Groningen Award \"Great interaction\" most unique game mechanics/controls)",
         cover: "./img/Other/logo.png",
@@ -21,6 +22,7 @@ const projects = [
     },
     {
         title: "Before Nightfall",
+        engine: "./img/Other/U_Cube_1C_Black.svg",
         category: "games",
         description: "About the game Before Nightfall (BNF for short). This game is a prototype game made for my study. BNF is a Tower defence game in a fantasy setting, you play as an carpenter for hire to defend someone's village or a castle. but where do you defend against? well there are multiple creatures like a oger, giant, demon and a boss creature like the slime or dragon. For each contract you complete you get gems which you can spend to upgrade the standard towers and ability's. there are 3 modes to the game" +
             "\n" +
@@ -48,6 +50,7 @@ const projects = [
     },
     {
         title: "Big Cheese Donte",
+        engine: "./img/Other/U_Cube_1C_Black.svg",
         category: "games",
         description:"This is a RPG prototype game based around a 1920 chicago mafia setting. You play as the son of a low ranking mafia boss. He has asked you to proof your capable of taking over his empire.\n" +
             "\n" +
@@ -69,6 +72,7 @@ const projects = [
     },
     {
         title: "Zombie Survivors",
+        engine: "./img/Other/U_Cube_1C_Black.svg",
         category: "games",
         description:"Zombie Survivors is a game based on the idea of the Vampire Survivors Game. The controls of the game are a bit unconventional that is because it was developed for an arcade for my school.",
         cover: "./img/Other/Zs.png",
@@ -87,6 +91,7 @@ const projects = [
     },
     {
         title: "Time Puzzler",
+        engine: "./img/Other/U_Cube_1C_Black.svg",
         category: "games",
         description:"For the Noorderpoort Game-jam 2024 i made this vr game in 4 days with 3 of my friends and fellow students, this was our first real try at a vr game and we have won 2 awards with this Game-jam: overall winner (first place/most votes) and most complete game (self explanatory) . We do want to work on it further but we need to find the time for it.",
         cover: "./img/Other/Time Puzz.webp",
@@ -154,11 +159,14 @@ function filterProjects(category) {
     filteredProjects.forEach(project => {
         const projectCard = document.createElement('div');
         projectCard.classList.add('project-card');
-        let badgesHTML = project.badges ? project.badges.map(badge => `<span class="badge" style="background-color: ${badge.color}">${badge.text}</span>`).join(' ') : '';
+        let badgesHTML = `<div class="badges-container">`;
+        badgesHTML += project.badges ? project.badges.map(badge => `<span class="badge" style="background-color: ${badge.color}">${badge.text}</span>`).join(' ') : '';
+        badgesHTML += `</div>`; // Close the badges container
+        let engineBadge = `<img src="${project.engine}" class="engine-badge" alt="Engine Logo">`;  // Use an <img> tag for the logo
         projectCard.innerHTML = `
+        ${engineBadge}
         ${badgesHTML}
         <h3>${project.title}</h3>
-        
         <img src="${project.cover}" alt="${project.title}">
         <p>${project.description}</p>
         <div class="project-links" style="font-size: xx-large">
@@ -171,6 +179,8 @@ function filterProjects(category) {
         container.appendChild(projectCard);
     });
 }
+
+
 
 
 function openSlideshow(index) {
