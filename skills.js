9bfa212748ceb4eb971c80fad3e1e7b116ecb569
Kornee Hartlief <90724234+JustMeGameDev@users.noreply.github.com>
2024-07-15 15:16:40 +0200

    update

--- skills.js
+++ skills.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     const defaultLang = localStorage.getItem('userLang') || 'en';
-    loadSkills(defaultLang);
+    switchLanguage(defaultLang); // Load initial language data on document ready
 
     document.getElementById('switchToEnglish').addEventListener('click', function() {
         switchLanguage('en');
@@ -9,24 +9,28 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('switchToDutch').addEventListener('click', function() {
         switchLanguage('nl');
     });
+
+    generateStars(200); // Generate stars for the background
 });
+
+let currentLang = 'en'; // Global variable to store the current language
 
 function switchLanguage(lang) {
     localStorage.setItem('userLang', lang); // Save the language preference
-    loadSkills(lang); // Load and render skills for the selected language
+    currentLang = lang; // Update the current language
+    loadLanguageSkills(lang); // Load and render skills for the selected language
 }
 
-function loadSkills(lang) {
-    const filepath = `./${lang}/skills_${lang}.xml`; // Adjust the path as needed
+function loadLanguageSkills(lang) {
+    const filepath = `./${lang}/skills_${lang}.xml`;
 
     fetch(filepath)
         .then(response => response.text())
         .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
         .then(data => {
             const skillSets = data.getElementsByTagName('skill-set');
-            const translations = data.getElementsByTagName('translation');
-            renderSkillButtons(skillSets); // Render skillset buttons
-            updateTextContent(translations); // Update static text according to the selected language
+            renderSkillButtons(skillSets);
+            applyTranslations(lang, data); // Apply translations after loading skills
         })
         .catch(err => console.error('Error loading the XML file:', err));
 }
@@ -41,10 +45,40 @@ function renderSkillButtons(skillSets) {
 
         const button = document.createElement('button');
         button.className = 'skill-button';
-        button.onclick = () => renderSkillSetDetails(skillSet);
         button.innerHTML = `<img src="${skillSetLogo}" alt="${skillSetName}" title="${skillSetName}" />`;
+        button.onclick = () => {
+            renderSkillSetDetails(skillSet);
+            renderTOC(skillSet);
+        };
 
         skillsContainer.appendChild(button);
+    });
+}
+
+function renderTOC(skillSet) {
+    const tocContainer = document.querySelector('.toc-container');
+    tocContainer.innerHTML = '<h3>Inhoudsopgave:</h3><ul style="list-style-type: disc;">';
+
+    const skills = skillSet.querySelectorAll('skills > skill');
+    Array.from(skills).forEach((skill, idx) => {
+        const skillName = skill.querySelector('name').textContent;
+        const listItem = document.createElement('li');
+        const link = document.createElement('a');
+        link.href = `#skill-title-${idx}`;
+        link.textContent = skillName;
+        link.addEventListener('click', (e) => {
+            e.preventDefault(); // Voorkomt de standaard hyperlink actie
+            const targetId = e.target.getAttribute('href');
+            const targetElement = document.querySelector(targetId);
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 50, // Verandert de offset naar wens
+                    behavior: 'smooth'
+                });
+            }
+        });
+        listItem.appendChild(link);
+        tocContainer.querySelector('ul').appendChild(listItem);
     });
 }
 
@@ -52,138 +86,109 @@ function renderSkillSetDetails(skillSet) {
     const detailsContainer = document.getElementById('skill-details');
     detailsContainer.innerHTML = ''; // Clear previous content
 
-    const skillSetName = skillSet.querySelector('name').textContent;
+    const skills = skillSet.querySelectorAll('skills > skill');
+    Array.from(skills).forEach((skill, idx) => {
+        const skillDetailDiv = document.createElement('div');
+        skillDetailDiv.id = `skill-detail-${idx}`;
 
-    // Create skill set title
-    const skillSetTitle = document.createElement('h2');
-    skillSetTitle.textContent = skillSetName;
-    detailsContainer.appendChild(skillSetTitle);
+        const title = document.createElement('h3');
+        title.id = `skill-title-${idx}`;
+        title.textContent = skill.querySelector('name').textContent;
+        skillDetailDiv.appendChild(title);
+        skillDetailDiv.appendChild(createSeparator());
 
-    // Create separator
-    const separator = document.createElement('div');
-    separator.className = 'border-line';
-    detailsContainer.appendChild(separator);
+        const description = document.createElement('p');
+        description.textContent = skill.querySelector('description').textContent;
+        skillDetailDiv.appendChild(description);
+        skillDetailDiv.appendChild(createSeparator());
 
-    const skillsList = skillSet.querySelectorAll('skills skill');
-
-    skillsList.forEach(subSkill => {
-        // Create skill title
-        const subSkillName = subSkill.querySelector('name').textContent;
-        const skillTitle = document.createElement('h3');
-        skillTitle.textContent = subSkillName;
-        detailsContainer.appendChild(skillTitle);
-
-        // Create smaller separator
-        const smallSeparator = document.createElement('div');
-        smallSeparator.className = 'small-border-line';
-        detailsContainer.appendChild(smallSeparator);
-
-        // Create progress bar
-        const level = subSkill.querySelector('level').textContent;
-        const progressBar = document.createElement('div');
-        progressBar.className = 'progress-bar';
-        progressBar.innerHTML = `
-            <div class="progress" style="width: ${level}%;"></div>
-        `;
-        detailsContainer.appendChild(progressBar);
-
-        // Create description
-        const description = subSkill.querySelector('description').textContent;
-        const desc = document.createElement('p');
-        desc.textContent = description;
-        detailsContainer.appendChild(desc);
-
-        const smallSeparator2 = document.createElement('div');
-        smallSeparator2.className = 'small-border-line';
-        detailsContainer.appendChild(smallSeparator2);
-
-        // Create media items
-        const media = subSkill.querySelectorAll('media item');
-        media.forEach(item => {
-            const mediaType = item.querySelector('type').textContent;
-            const src = item.querySelector('src').textContent;
-            const mediaDesc = item.querySelector('description').textContent;
-
-            const mediaDiv = document.createElement('div');
-            mediaDiv.className = 'media-item';
-
-            if (mediaType === 'image') {
-                mediaDiv.innerHTML = `<img src="${src}" alt="${mediaDesc}" />`;
-            } else if (mediaType === 'video') {
-                mediaDiv.innerHTML = `
-                    <video controls>
-                        <source src="${src}" type="video/mp4">
-                        Your browser does not support the video tag.
-                    </video>
-                `;
-            }
-
-            detailsContainer.appendChild(mediaDiv);
+        const mediaItems = skill.querySelectorAll('media > item');
+        const mediaList = document.createElement('div');
+        mediaList.style.textAlign = "center"; // Center media items
+        mediaItems.forEach(item => {
+            mediaList.appendChild(createMediaItem(item));
+            mediaList.appendChild(createSeparator());
         });
 
-        // Create smaller separator after each skill
-        const endSeparator = document.createElement('div');
-        endSeparator.className = 'small-border-line';
-        detailsContainer.appendChild(endSeparator);
+        skillDetailDiv.appendChild(mediaList);
+        detailsContainer.appendChild(skillDetailDiv);
     });
 }
 
-function updateTextContent(translations) {
-    var currentYear = new Date().getFullYear();
-    var previousYear = 2023;
-    var yearText = `${previousYear}-${currentYear}`; // Generates "2023-2024"
+function createMediaItem(item) {
+    const itemType = item.querySelector('type').textContent;
+    const itemSrc = item.querySelector('src').textContent;
+    const itemDescription = item.querySelector('description').textContent;
 
+    let mediaElement;
+    if (itemType === 'image') {
+        mediaElement = document.createElement('img');
+        mediaElement.src = itemSrc;
+        mediaElement.alt = itemDescription;
+        mediaElement.style.maxWidth = '100%';
+    } else if (itemType === 'video') {
+        mediaElement = document.createElement('video');
+        mediaElement.controls = true;
+        const source = document.createElement('source');
+        source.src = itemSrc;
+        source.type = 'video/mp4';
+        mediaElement.appendChild(source);
+        mediaElement.innerHTML += 'Your browser does not support the video tag.';
+    }
+
+    const mediaDiv = document.createElement('div');
+    mediaDiv.className = 'media-item';
+    mediaDiv.style.margin = "0 auto"; // Center media
+    mediaDiv.appendChild(mediaElement);
+    const caption = createTextElement('p', itemDescription);
+    caption.style.textAlign = "left"; // Left-align the text description
+    mediaDiv.appendChild(caption);
+
+    return mediaDiv;
+}
+
+function createTextElement(tag, text) {
+    const element = document.createElement(tag);
+    element.textContent = text;
+    return element;
+}
+
+function createSeparator() {
+    const separator = document.createElement('div');
+    separator.className = 'separator';
+    return separator;
+}
+
+function applyTranslations(lang, xmlDoc) {
     document.querySelectorAll("[data-translation-id]").forEach(element => {
         const key = element.getAttribute('data-translation-id');
-        const translationNode = Array.from(translations).find(tr => tr.getAttribute('id') === key);
+        const translationNode = Array.from(xmlDoc.getElementsByTagName("translation")).find(tr => tr.getAttribute('id') === key);
         if (translationNode) {
-            var text = translationNode.textContent.replace('{year}', yearText);
-            element.textContent = text;
+            element.textContent = translationNode.textContent;
         }
     });
 
-    // Update the current year in the footer
-    document.getElementById('currentYear').textContent = currentYear;
+    const skillSets = xmlDoc.getElementsByTagName('skill-set');
+    renderSkillButtons(skillSets); // Herlaad de knoppen zodat deze ook vertaald worden
 }
 
-generateStars(200); // Generate 200 stars. Adjust the number as needed.
-
-document.querySelectorAll('.header-img').forEach(img => {
-    img.addEventListener('mouseenter', function () {
-        // Get the tooltip element
-        const tooltip = document.querySelector('.header-tooltip');
-        // Set the tooltip text to the title of the image
-        tooltip.textContent = this.title;
-        // Optional: make the tooltip visible if it's hidden by default
-        tooltip.style.opacity = 1;
-    });
-
-    img.addEventListener('mouseleave', function () {
-        // Clear the tooltip text when not hovering
-        const tooltip = document.querySelector('.header-tooltip');
-        tooltip.style.opacity = 0;
-
-        // Optional: hide the tooltip when not hovering
-    });
-});
-
-function getRandomColor() {
-    const colors = ['#FFFFFF', '#F0F8FF', '#E0FFFF', '#E6E6FA', '#F8F8FF']; // Example star colors
-    return colors[Math.floor(Math.random() * colors.length)];
-}
 
 function generateStars(numberOfStars) {
     const sky = document.getElementById('starry-sky');
-
     for (let i = 0; i < numberOfStars; i++) {
         let star = document.createElement('div');
         star.style.position = 'absolute';
         star.style.left = `${Math.random() * 100}%`;
         star.style.top = `${Math.random() * 100}%`;
-        star.style.width = `${Math.random() * 7}px`; // Stars size between 1px and 3px
-        star.style.height = star.style.width; // Keep the star size consistent
+        star.style.width = `${Math.random() * 3}px`;
+        star.style.height = star.style.width;
         star.style.borderRadius = '50%';
         star.style.backgroundColor = getRandomColor();
         sky.appendChild(star);
     }
+}
+
+function getRandomColor() {
+    const colors = ['#FFFFFF', '#F0F8FF', '#E0FFFF', '#E6E6FA', '#F8F8FF'];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
